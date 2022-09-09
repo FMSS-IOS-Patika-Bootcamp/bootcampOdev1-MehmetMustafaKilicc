b06c20f4fce9ae5d73b2f1f9fd301e70e94e58e6
Mehmet Mustafa Kılıç <muh.mstf.klc@gmail.com>
2022-09-09 11:15:09 +0300

    Initial Commit

---
+++ bootcampOdev1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bootcampOdev1
+//
+//  Created by Mehmet Mustafa Kılıç on 9.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
